690735dacc5211320445d5b5685baa8135ff64e3
John W. Terrell <john@coolpeoplenetworks.com>
2021-12-06 08:17:05 -0800

    WIP

--- src/node.rs
+++ src/node.rs
@@ -1,16 +1,20 @@
 use std::iter::FusedIterator;
 use std::mem;
+use std::sync::{
+    Arc,
+};
 
+// see https://aloso.github.io/2021/03/09/creating-an-iterator
 pub struct Node<T> {
     contents: T,
-    children: Vec<Node<T>>,
+    children: Arc<Vec<Node<T>>>,
 }
 
 impl<T> Node<T> {
     pub fn new(contents: T, children: Vec<Node<T>>) -> Self {
         Self {
             contents,
-            children,
+            children: Arc::new(children),
         }
     }
 }
@@ -51,20 +55,6 @@ impl<'a, T> core::iter::Iterator for Iterator<'a, T> {
                 };
                 Some(&node.contents)
             },
-            // Some(Node::Leaf(item)) => {
-            //     self.children = &self.children[1..];
-            //     Some(item)
-            // }
-            // Some(Node::Children(children)) => {
-            //     self.children = &self.children[1..];
-
-            //     // start iterating the child trees
-            //     *self = NodeIter {
-            //         children: children.as_slice(),
-            //         parent: Some(Box::new(mem::take(self))),
-            //     };
-            //     self.next()
-            // }
         }
     }
 }
@@ -81,6 +71,7 @@ impl<'a, T> FusedIterator for Iterator<'a, T> {
 #[cfg(test)]
 mod tests {
     use super::{Node};
+    use std::sync::Arc;
 
     #[test]
     fn iteration() {
@@ -110,5 +101,47 @@ mod tests {
 
         let nums: Vec<i32> = tree.iter().copied().collect();
         assert_eq!(nums, vec![0, 1, 2, 3, 4, 40, 41, 42, 420, 421, 5, 6, 60, 61, 62, 620, 621, 7]);
+    }
+
+    #[test]
+    fn multithreaded_iteration() {
+        let tree = Arc::new(Node::new(0, vec![
+                Node::new(1, vec![]),
+                Node::new(2, vec![]),
+                Node::new(3, vec![]),
+                Node::new(4, vec![
+                    Node::new(40, vec![]),
+                    Node::new(41, vec![]),
+                    Node::new(42, vec![
+                        Node::new(420, vec![]),
+                        Node::new(421, vec![]),    
+                    ]),
+                ]),
+                Node::new(5, vec![]),
+                Node::new(6, vec![
+                    Node::new(60, vec![]),
+                    Node::new(61, vec![]),
+                    Node::new(62, vec![
+                        Node::new(620, vec![]),
+                        Node::new(621, vec![]),    
+                    ]),
+                ]),
+                Node::new(7, vec![]),
+            ])
+        );
+
+        let mut handles = vec![];
+        for _ in 0..10 {
+            let tree = Arc::clone(&tree);
+            let handle = std::thread::spawn(move || {
+                let nums: Vec<i32> = tree.iter().copied().collect();
+                assert_eq!(nums, vec![0, 1, 2, 3, 4, 40, 41, 42, 420, 421, 5, 6, 60, 61, 62, 620, 621, 7]);
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
     }
 }
